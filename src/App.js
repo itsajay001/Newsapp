eef0262867f7d2850d13f4339a449c8578032082
itsajay001 <133009231+itsajay001@users.noreply.github.com>
2024-06-28 08:30:44 +0530

    added loaading logic

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Navbar from './Components/Navbar'
 import News from './Components/News'
 
 export class App extends Component {
+ 
 
   render() {
     return (<>
       <Navbar/>
-      <News pageSize={5} country={"in"}/>
+      <News pageSize={5} country={"in"} category={"science"}/>
       
       </>
     )
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types'
-
+import PropTypes from "prop-types";
 
 export class News extends Component {
   static defaultProps = {
-    country: 'in',
-    pageSize: 5
-  }
+    country: "in",
+    pageSize: 5,
+    category: "general",
+  };
 
   static propTypes = {
     country: PropTypes.string,
-    pageSize: PropTypes.number
-  }
-
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
 
   constructor() {
     super();
@@ -27,7 +27,7 @@ export class News extends Component {
     //this is by default page 1 state
   }
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&apiKey=b90b723723564ab4ba42b53af11e9440&page=1&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b90b723723564ab4ba42b53af11e9440&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
@@ -37,7 +37,11 @@ export class News extends Component {
   }
 
   Preclick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&apiKey=b90b723723564ab4ba42b53af11e9440&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=b90b723723564ab4ba42b53af11e9440&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
@@ -62,7 +66,11 @@ export class News extends Component {
         Math.ceil(this.state.totalResults / this.props.pageSize)
       )
     ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&apiKey=b90b723723564ab4ba42b53af11e9440&page=${
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=b90b723723564ab4ba42b53af11e9440&page=${
         this.state.page + 1
       }&pageSize=${this.props.pageSize}`;
       this.setState({ loading: true });
@@ -71,7 +79,7 @@ export class News extends Component {
       this.setState({ articles: parsedData.articles });
 
       this.setState({
-        page: this.state.page + 1, //
+        page: this.state.page + 1, 
         loading: false,
       });
     }
@@ -83,7 +91,8 @@ export class News extends Component {
         <h1 className="text-center">News Headline</h1>
         {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles && !this.state.loading &&  //if loading is false then only show the article
+          {this.state.articles &&
+            !this.state.loading && //if loading is false then only show the article
             this.state.articles.map((element) => {
               return (
                 <div className="col-md-3" key={element.url}>
@@ -108,7 +117,7 @@ export class News extends Component {
         <div className="container d-flex justify-content-between">
           <button
             type="button"
-            class="btn btn-dark"
+            className="btn btn-dark"
             onClick={this.Preclick}
             disabled={this.state.page <= 1} //page is already set in this.state which is 1
           >
@@ -119,7 +128,7 @@ export class News extends Component {
               this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
             }
             type="button"
-            class="btn btn-dark"
+            className="btn btn-dark"
             onClick={this.Nextclick}
           >
             Next &raquo;
